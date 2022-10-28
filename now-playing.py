8152d69f792be4e1c8261c798c0b15c41964956c
Sunghyun Cho <mail@chosunghyun.com>
2022-10-28 16:33:53 -0700

    init directory

---
+++ now-playing.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+url: str = 'https://api.music.apple.com/v1/me/recent/played/tracks'
+
+headers: dict = {
+    'Authorization': f'Bearer {os.getenv("APPLE_MUSIC_DEV_TOKEN")}',
+    'Music-User-Token': os.getenv('APPLE_MUSIC_USER_TOKEN'),
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+}
+
+
+def get_now_playing():
+    import requests
+    return requests.get(url, headers=headers)
+
+
+if __name__ == '__main__':
+    get_now_playing().json()
+    # write to file, pretty print, etc.
+    import json
+    with open('now-playing.json', 'w') as f:
+        json.dump(get_now_playing().json(), f, indent=2)
