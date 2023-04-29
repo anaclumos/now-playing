8bb3196609ebbb254a10f6ee4af8dfd6e9d76dbf
Sunghyun Cho <mail@chosunghyun.com>
2023-04-29 09:34:38 -0700

    Refactor now-playing.py to use consistent quotation marks and improve code readability

--- now-playing.py
+++ now-playing.py
@@ -3,28 +3,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-url: str = 'https://api.music.apple.com/v1/me/recent/played/tracks'
+url: str = "https://api.music.apple.com/v1/me/recent/played/tracks"
 
 headers: dict = {
-    'Authorization': f'Bearer {os.environ["APPLE_MUSIC_DEV_TOKEN"]}',
-    'Music-User-Token': os.environ['APPLE_MUSIC_USER_TOKEN'],
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
+    "Authorization": f'Bearer {os.environ["APPLE_MUSIC_DEV_TOKEN"]}',
+    "Music-User-Token": os.environ["APPLE_MUSIC_USER_TOKEN"],
+    "Accept": "application/json",
+    "Content-Type": "application/json",
 }
 
 
 def get_now_playing():
     import requests
+
     try:
         result = requests.get(url, headers=headers)
     except requests.exceptions.RequestException as e:
+        print(e)
         return None
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import json
-    with open('now-playing.json', 'w') as f:
+
+    with open("now-playing.json", "w") as f:
         result = get_now_playing()
         if result is not None:
             json.dump(result.json(), f, indent=2)
